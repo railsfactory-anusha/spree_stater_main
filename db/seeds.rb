94250bb87bbbc5c6f2f99ddd19ca14e7e1d1552d
anusha <anusha@railsfactory.org>
2021-12-15 22:44:53 +0530

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+#
+# Place all seeds in /seeds/ folder.
+#
+Dir[File.dirname(__FILE__) + '/seeds/*.rb'].sort.each do |file|
+  puts "Seeds #{file} ..."
+  require file
+end
+
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
